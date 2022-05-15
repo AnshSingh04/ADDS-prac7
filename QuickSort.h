9165bd4c165790c7e7629c732b975f0d9a7f5fb2
AnshSingh04 <73479734+AnshSingh04@users.noreply.github.com>
2022-05-15 17:02:21 +0530

    Add files via upload

--- QuickSort.h
+++ QuickSort.h
@@ -9,8 +9,7 @@ class QuickSort: public Sort {
     
     public:
     vector<int> sort(vector<int> list);
-    int partition(vector<int>&list, int low, int high);
-    void quicksrt(vector<int>&list,int low, int high);
+    vector<int> quicksrt(vector<int>list,int low, int high);
     
 };
 #endif
