f8bc9dd0da4cef70079799bdebe9ba8d805bbc53
AnshSingh04 <73479734+AnshSingh04@users.noreply.github.com>
2022-05-15 16:38:39 +0530

    Add files via upload

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 vector<int> BubbleSort::sort(vector<int> list) {
     int len = list.size();
+    if(len == 1)
+        return list;
     for (int i = 0; i < len-1; i++) {
         for (int j = 0; j < len-i-1; j++) {
             if (list.at(j) > list.at(j + 1))
