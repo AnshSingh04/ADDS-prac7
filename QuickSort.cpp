2f677d71ce1abd1ca006a472574fa39db8fe7c0f
AnshSingh04 <73479734+AnshSingh04@users.noreply.github.com>
2022-05-15 16:14:26 +0530

    Add files via upload

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -21,8 +21,6 @@ void QuickSort::quicksrt(vector<int>&list, int low, int high) {
 int QuickSort::partition(vector<int>&list, int low, int high) {
   int pivot = high;
   int j = low;
-  if(list.size()>=3)
-    swap(list.at(2),list.at(high));
 	for(int i=low;i<high;++i){
 		if(list.at(i)<list.at(pivot)){
 			swap(list.at(i),list.at(j));
